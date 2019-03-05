b1fe1533b7b7347ba13d65b46b804c08f1a424e4
Brian Vogelsang <info@bvogelsang.com>
2019-03-05 12:38:20 -0600

    updatesoundroom

--- projects/read/js/readingroom.js
+++ projects/read/js/readingroom.js
@@ -26,10 +26,14 @@ var rain = document.getElementById("myRain");
 var fire = document.getElementById("myFire");
 var library = document.getElementById("myLibrary");
 var confirm = document.getElementById("currentPlaylistText");
+var startBtn = document.getElementById("JungleRoomStartBtn");   
+var stopBtn = document.getElementById("JungleRoomStopBtn"); 
     
 confirm.innerHTML='Starting: Jungle Room';
 
-library.pause();
+startBtn.style.display='none';
+stopBtn.style.display='block'; 
+    
     
 rain.loop = true;
 rain.volume =0.2;
@@ -47,16 +51,32 @@ fire.controls = true;
     
 }
 
-function startLibrary(){
+function stopJungleRoom() {
 var rain = document.getElementById("myRain");
 var fire = document.getElementById("myFire");
+var confirm = document.getElementById("currentPlaylistText");
+var startBtn = document.getElementById("JungleRoomStartBtn");   
+var stopBtn = document.getElementById("JungleRoomStopBtn"); 
+    
+confirm.innerHTML='Stopping Jungle Room';
+
+startBtn.style.display='block';
+stopBtn.style.display='none';     
+    
+rain.pause();
+fire.pause();  
+}
+
+function startLibrary(){
 var library = document.getElementById("myLibrary");
 var confirm = document.getElementById("currentPlaylistText");
+var startBtn = document.getElementById("LibraryRoomStartBtn");   
+var stopBtn = document.getElementById("LibraryRoomStopBtn");      
     
 confirm.innerHTML='Starting: Library';
-
-rain.pause();
-fire.pause();
+    
+startBtn.style.display='none';
+stopBtn.style.display='block';     
     
 library.loop = true;
 library.volume =0.5;
@@ -65,6 +85,20 @@ library.autoplay = true;
 library.play();
 library.controls =true;
     
+}
+function stopLibrary(){
+var library = document.getElementById("myLibrary");
+var confirm = document.getElementById("currentPlaylistText");
+var startBtn = document.getElementById("LibraryRoomStartBtn");   
+var stopBtn = document.getElementById("LibraryRoomStopBtn");    
+    
+confirm.innerHTML='Stopping Library Sounds';
+    
+startBtn.style.display='block';
+stopBtn.style.display='none';       
+    
+
+library.pause();
 }
 
 
